8488a19a849157298101e59ce840396bf8fc3952
NavjotSaroa <77352263+NavjotSaroa@users.noreply.github.com>
2024-03-21 21:53:42 -0600

    Update Homepage.js

--- my-ecommerce-app/src/component/Homepage.js
+++ my-ecommerce-app/src/component/Homepage.js
@@ -1,7 +1,6 @@
-// src/components/Homepage.js
 import React from 'react';
 import Header from './Header';
-import HomeMainSection from './HomeMainSection'; // Ensure this path is correct
+import HomeMainSection from './HomeMainSection';
 import Footer from './Footer';
 
 function Homepage() {
